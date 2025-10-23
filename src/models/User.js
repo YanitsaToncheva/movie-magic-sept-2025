ed8bdef4c2614678fdf22f56a2b29791468ea58a
Yanitsa Toncheva <yanitsa.toncheva87@gmail.com>
2025-10-23 13:55:26 +0300

    Add password hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,7 @@
 import { Schema, model} from 'mongoose';
 
+import bcrypt from 'bcrypt';
+
 const userSchema = new Schema({
     email: {
         type: String,
@@ -10,6 +12,12 @@ const userSchema = new Schema({
         required: true,
     }
 });
+
+userSchema.pre('save', async function () {
+    //const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, 13);
+
+})
 
 const User = model('User', userSchema);
 
